dcdcd88bd7f78b7ebb4daf0221c7895b4b4ee803
Samuel Ayinde <ayindesamuel0@gmail.com>
2021-10-02 10:33:51 +0100

    React native onboarding screen UI

---
+++ constants/images.js
@@ -0,0 +1,7 @@
+const onBoarding1 = require("../assets/images/onboarding-1.png");
+const onBoarding2 = require("../assets/images/onboarding-2.png");
+const onBoarding3 = require("../assets/images/onboarding-3.png");
+
+// const appImages = { onBoarding1, onBoarding2, onBoarding3 };
+
+export default { onBoarding1, onBoarding2, onBoarding3 };
